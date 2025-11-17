ccfae264aa5233bf447717c9d6e911aa3bbb9af7
nyanyacyan <nyanyacyan@gmail.com>
2025-11-18 06:54:34 +0900

    configから値を読み込むメソッドを構築

---
+++ python-app/src/utils/read_config.py
@@ -0,0 +1,105 @@
+# coding: utf-8
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# import
+import json
+import os
+
+from logger import Logger
+
+# ----------------------------------------------------------------------------------
+# **********************************************************************************
+
+class ReadConfig:
+    def __init__(self):
+        # logger
+        self.getLogger = Logger()
+        self.logger = self.getLogger.getLogger()
+
+# ----------------------------------------------------------------------------------
+# 実施結果を返すメソッド
+
+    def process(self, key: str, detail_key: str):
+        self.logger.debug("configの値の読込開始")
+
+        value = self.get_json_value(key, detail_key)
+
+        self.logger.debug("configの値の読込終了")
+        return value
+
+# ----------------------------------------------------------------------------------
+# 現在のカレントディレクトリを取得するメソッド
+
+    def currentDir(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.logger.debug(f"Current directory: {current_dir}")
+        return current_dir
+
+# ----------------------------------------------------------------------------------
+# configのパスを取得するメソッド
+
+    def get_config_path(self):
+        base_dir = self.currentDir()
+        config_path = os.path.join(base_dir, 'data', 'config', 'config.json')
+        self.logger.debug(f"Config path: {config_path}")
+        return config_path
+
+# ----------------------------------------------------------------------------------
+# jsonファイルを読み込むメソッド
+
+    def load_config(self):
+        config_path = self.get_config_path()
+        try:
+            with open(config_path, 'r', encoding='utf-8') as config_file:
+                config = json.load(config_file)
+                self.logger.info("Configuration loaded successfully.")
+                return config
+        except FileNotFoundError as e:
+            self.logger.critical(f"configファイルが見つかりませんでした: {config_path}")
+            raise FileNotFoundError(f"configファイルが見つかりませんでした: {config_path}") from e
+
+        except json.JSONDecodeError as e:
+            self.logger.critical(f"configファイルのJSONデコードエラーが発生しました: {config_path}")
+            raise ValueError(f"configファイルのJSONデコードエラーが発生しました: {config_path}") from e
+
+# ----------------------------------------------------------------------------------
+# jsonファイルから値を取得するメソッド
+
+    def get_json_value(self, key: str, detail_key: str):
+        config = self.load_config()
+        try:
+            values = config[key]
+            self.logger.debug(f"'config[{key}]': {values}")
+            value = values[detail_key]
+            masked_value = self.mask_value(value)
+            self.logger.info(f"configから取得した内容 '{detail_key}': {masked_value}")
+            return masked_value
+
+        except KeyError as e:
+            self.logger.error(f"指定されたキーがconfigファイルに存在しません: {key}")
+            raise KeyError(f"指定されたキーがconfigファイルに存在しません: {key}") from e
+
+# ----------------------------------------------------------------------------------
+# 値を隠すメソッド
+
+    def mask_value(self, value: str, unmasked: int =2):
+        if not isinstance(value, str):
+            mask_value = "****"
+        if len(value) <= unmasked:
+            mask_value = value + "*" * 4     # かなり短い場合のケア
+        mask_value[:unmasked] + "*" * (len(value) - unmasked)
+        self.logger.debug(f"Masked value: {mask_value}")
+        return mask_value
+
+# ----------------------------------------------------------------------------------
+
+
+# **********************************************************************************
+
+if __name__ == "__main__":
+    key = "CHATWORK"
+    detail_key = "ROOM_ID"
+    ReadConfig().process(key=key, detail_key=detail_key)
+# ----------------------------------------------------------------------------------
