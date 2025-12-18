c4f382c085f07619ec3c1737d07bd601dfc1855a
にゃにゃちゃん <131307096+nyanyacyan@users.noreply.github.com>
2025-12-18 13:16:04 +0900

    no message

--- src/infrastructure/chatwork/config.py
+++ src/infrastructure/chatwork/config.py
@@ -20,6 +20,8 @@ from pydantic_settings import BaseSettings
 class ChatworkConfig(BaseSettings):
     chatwork_api_token: str
     chatwork_endpoint_url: str = "https://api.chatwork.com/v2"
+    chatwork_send_room_id: int
+    chatwork_reception_room_id: int
     
     class Config:
         env_file = ".env.chatwork"
